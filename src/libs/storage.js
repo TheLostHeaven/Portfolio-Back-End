605183b0d8d43b3845b8df9d0ccceca53049b49f
Daniel <dannyking07@outlook.es>
2023-10-14 07:55:01 -0500

    feat [src/libs -> storage.js]

---
+++ src/libs/storage.js
@@ -0,0 +1,22 @@
+import multer from 'multer';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadDir = path.join(__dirname, '../uploads/');
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
