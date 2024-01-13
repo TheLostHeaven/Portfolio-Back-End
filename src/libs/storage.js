3f9a4a26c5bea98ab2a1a712fee6bc87f4415945
Daniel <Dannyking07@outlook.es>
2024-01-13 18:36:44 -0500

    feat Upload -> Files

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -9,7 +9,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadDir = path.join(__dirname, '../uploads/');
+    const uploadDir = path.join(__dirname, '../Files/');
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
