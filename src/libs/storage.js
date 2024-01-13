05e4f174a52f0d17bef1d225b080e6c8cabd9042
Daniel <Dannyking07@outlook.es>
2024-01-13 18:03:14 -0500

    now take original name in te files -> storage.js

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, file.originalname);
   },
 });
 
