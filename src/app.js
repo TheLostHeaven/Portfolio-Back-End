4c6141afd765ca2c9360f366e66b78cce40a226f
Daniel Molina <90277397+TheLostHeaven@users.noreply.github.com>
2024-01-14 00:38:14 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,6 +36,11 @@ app.use(
       optionsSuccessStatus: 204,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // actualiza con el dominio que necesites
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 export default app;
