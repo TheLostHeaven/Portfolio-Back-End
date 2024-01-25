e77c24a242de75abfd5d0de24a4c15c2e3962d87
Daniel <Dannyking07@outlook.es>
2024-01-25 12:29:25 -0500

    modified:   src/Schemas/Img.js

--- src/Schemas/Img.js
+++ src/Schemas/Img.js
@@ -2,21 +2,16 @@ import mongoose from "mongoose";
 
 const imgSchema = new mongoose.Schema({
 
-  imgId: {
-    type: String
-  },
-
   imgfile: {
     type: String
   },
-  
+
   uploadedAt: {
     type: Date,
     default: Date.now,
   },
 },
 {
-  _id: false,
   timestamps: true,
   versionKey: false
 });
